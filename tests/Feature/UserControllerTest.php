dc6162c775a2ffea9371dc3e3566e4c5b8270472
saitarun135 <saitarun135@gm.com>
2021-06-26 08:56:37 +0530

    test: ForgotPassword testcases are added for both sucess and failure case

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -46,5 +46,47 @@ class UserControllerTest extends TestCase
         ]);
         $response->assertStatus(200);
     }
-   
+    /**
+     * if guest (without credentials)login it shows unprocessible entity 
+     */
+    public function test_Login_Guest()
+    {
+        $response = $this->json('POST','/api/login',[
+        ]);
+        $response->assertStatus(422);
+    }
+    /* 
+    * if the user provides invalid credentials
+    */
+    public function test_Login_With_Invalid_Credentials()
+    {
+        $response = $this->json('POST','/api/login',[
+            "email"=>"saitarun800@gmail.com",
+            "password"=>"SAItarun*123"
+        ]);
+        $response->assertStatus(401);
+    }
+    /**
+     * checks emailID with DB and create a success msg
+    */
+    public function test_ForgotPasswordCreateSuccess(){
+        $response = $this->withHeaders([
+            'content-Type' =>'Application/json'
+            ])->json('POST','/api/auth/sendPasswordResetLink',[
+               'email' => 'saitarun800@gmail.com'
+            ]);
+        $response->assertStatus(200)
+            ->assertExactJson(['data' => 'Reset link is send successfully, please check your inbox.']);
+    }
+    /**
+    * checks weather the email is valid or not 
+    */
+    public function test_ForgotPasswordCreateFailure(){
+        $response = $this->withHeaders([
+            'Content-Type' => 'Application/Json'
+            ])->json('Post','/api/auth/forgot',[
+                'email' =>'johnjo@gmail.com'
+            ]);
+        $response->assertStatus(404);       
+        }    
 }
