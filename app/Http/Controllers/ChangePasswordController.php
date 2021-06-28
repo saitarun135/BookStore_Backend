dafb5ee70be7b963ea5a702c40ee9ab7cdf34f47
saitarun135 <saitarun135@gm.com>
2021-06-28 16:03:21 +0530

    refactor: verification by Authorization token and removed email

--- app/Http/Controllers/ChangePasswordController.php
+++ app/Http/Controllers/ChangePasswordController.php
@@ -7,31 +7,37 @@ use App\Http\Requests\UpdatePasswordRequest;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
+use App\Models\PasswordReset;
+use Illuminate\Support\Facades\Validator as FacadesValidator;
 
 class ChangePasswordController extends Controller
 {
-    public function passwordResetProcess(UpdatePasswordRequest $request){
-        return $this->updatePasswordRow($request)->count() > 0 ? $this->resetPassword($request) : $this->tokenNotFoundError();
-      }
-      private function updatePasswordRow($request){
-         return DB::table('password_resets')->where([
-            'email' => $request->email,
-             'token' => $request->resetToken
-         ]);
-      } 
-      private function tokenNotFoundError() {
-          return response()->json([
-            'error' => 'Either your email or token is wrong.'
-          ],Response::HTTP_UNPROCESSABLE_ENTITY);
-      }
-      private function resetPassword($request) {
-          $userData = User::whereEmail($request->email)->first();
-          $userData->update([
-            'password'=>bcrypt($request->password)
-          ]);
-          $this->updatePasswordRow($request)->delete();
-          return response()->json([
-            'data'=>'Password has been updated.'
-          ],Response::HTTP_CREATED);
-      } 
+    public function resetPassword(Request $request)
+    {
+        $validate = FacadesValidator::make($request->all(), [
+            'new_password' => 'min:6|required|regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/',
+            'confirm_password' => 'required|same:new_password'
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json(['message' => "Password doesn't match"]);
+        }
+        $passwordReset = PasswordReset::where([
+            ['token', $request->bearerToken()]])->first();
+        
+            if (!$passwordReset) {           
+            return response()->json([ 'message' => 'This token is invalid'], 201);
+            }
+        $user = User::where('email', $passwordReset->email)->first();
+        
+        if (!$user) {
+            return response()->json(['message' => "we can't find the user with that e-mail address"], 201);
+        } 
+        else {
+            $user->password = bcrypt($request->new_password);
+            $user->save();
+            $passwordReset->delete();
+            return response()->json(['data'=>'Password has been updated.'],Response::HTTP_CREATED);
+        }
+    }
 }
