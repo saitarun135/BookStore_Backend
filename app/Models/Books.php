1029328eefe0d5cd6b102213a5d493620b6253cf
saitarun135 <saitarun135@gm.com>
2021-06-30 09:27:17 +0530

    build: books model added without image insertion

---
+++ app/Models/Books.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Books extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        // 'image',
+        'price',
+        'title',
+        'quantity',
+        'ratings',
+        'author'   
+    ];
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'updated_at'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+   
+    //inverse one to many
+   public function user(){
+       return $this->belongsTo(User::class);
+   }
+}
