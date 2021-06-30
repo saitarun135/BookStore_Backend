7e6538e0e02a02b482e96267e8516d1ae0b73d55
saitarun135 <saitarun135@gm.com>
2021-06-30 09:31:09 +0530

    feat: handle incoming request method added

---
+++ app/Http/Middleware/JWTMiddleWare.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+// use JWtAuth;
+use Illuminate\Http\Request;
+// use Tymon\JWTAuth\JWTAuth;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class JWTMiddleWare
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = JWTAuth::parseToken()->authenticate();
+        return $next($request);
+    }
+}
