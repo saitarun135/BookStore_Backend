c33aa9c08c410d2b07cee719bb88424f191260aa
saitarun135 <saitarun135@gm.com>
2021-06-30 09:29:23 +0530

    build: migration table for books without image column

---
+++ database/migrations/2021_06_29_153445_create_books_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->length(50)->unique();
+           // $table->binary('image')->nullable();
+            $table->integer('price')->unsigned();
+            $table->text('title');
+            $table->integer('quantity')->length(2)->unsigned();
+            $table->integer('ratings')->length(2)->unsigned();
+            $table->string('author');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
