44a4482c3115cee6f3b1885ba5c0ecf7d7f3a079
saitarun135 <saitarun135@gm.com>
2021-07-09 08:35:02 +0530

    build: search and sort apis added

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -5,29 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Books;
 use App\Models\User;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Auth;
-use SebastianBergmann\CodeCoverage\StaticAnalysis\Cache;
+
 use App\Http\Resources\Books as BookResource;
 
 class FileController extends Controller
 {
-    public function index()
-    {
-        $url = 'https://s3.' . env('AWS_DEFAULT_REGION') . '.amazonaws.com/' . env('AWS_BUCKET') . '/';
-        $images = [];
-        $files = Storage::disk('s3')->files('file');
-            foreach ($files as $file) {
-                $images[] = [
-                        'name' => str_replace('apiDocs/', '', $file),
-                        'src' => $url . $file
-                    ];
-                    }
-            return ['welcome'];
-    }
-
+    
     public function upload(Request $request ){
         $book = new Books();
         $book->price=$request->input('price');
@@ -35,20 +19,10 @@ class FileController extends Controller
         $book->quantity=$request->input('quantity');
         $book->author=$request->input('author');
         $book->description=$request->input('description');
-        $date = Carbon::now();
-        $file=$request->file;
-        $filePath = '/images/books/' . $date->year;
-        $filename = $date->timestamp . '_' . $file->getClientOriginalName();
-        $book->file = $file->storeAs($filePath, $filename, 'public');;
+        $book->file=$request->input('file');
         $book->user_id = auth()->id();
         $book->save();
         return new BookResource($book);
-    }
-  
-    public function destroy($image)
-    {
-        Storage::disk('s3')->delete('apiDocs/' . $image);
-        return ['Image was deleted successfully'];
     }
 
     public function display_Book($id)
@@ -59,6 +33,37 @@ class FileController extends Controller
         else{
             return response()->json(['error' => 'UnAuthorized/invalid id'], 401);    
             }
+    }
+
+    public function searchbooks($name)
+    {
+        $books=Books::all();
+        if(User::find($books->user_id=auth()->id())->books){
+             $searchBooks=  Books::where("name",$name)->get();
+             return response()->json(['books' => $searchBooks], 200);
+        }
+        else{
+            return response()->json(['error'=>'no books '],404);
+        }
+    }
+
+    public function sortBooksHighToLow(){
+        $books=Books::all();
+        if(User::find($books->user_id=auth()->id())->books){
+            $returnBooks=Books::orderBy('price','DESC')->get();
+            return response()->json(['books'=>$returnBooks],200);
+        }
+        else{
+            return response()->json(['error'=>'error'],401);
+        }
+    }
+
+    public function sortBooksLowToHigh(){
+        $books=Books::all();
+        if(User::find($books->user_id=auth()->id())->books){
+            $returnBooks=Books::orderBy('price','ASC')->get();
+            return response()->json(['books'=>$returnBooks],200);
+        }
     }
 
     public function displayBooks()
@@ -75,11 +80,13 @@ class FileController extends Controller
                                'price' => $request->input('price'),
                                'author' => $request->input('author'),
                                'description'=>$request->input('description'),
-                               'quantity'=>$request->input('quantity')
+                               'quantity'=>$request->input('quantity'),
+                               'file'=>$request->input('file')
             ));
             return['updated successfully'];
         }
     }
+
     public function DeleteBook($id)
     {
         $book=Books::findOrFail($id);
@@ -93,4 +100,25 @@ class FileController extends Controller
                 'error' => ' Method Not Allowed/invalid Book id'], 405);
         }
     }
+
+    public function index()
+    {
+        $url = 'https://s3.' . env('AWS_DEFAULT_REGION') . '.amazonaws.com/' . env('AWS_BUCKET') . '/';
+        $images = [];
+        $files = Storage::disk('s3')->files('file');
+            foreach ($files as $file) {
+                $images[] = [
+                        'name' => str_replace('apiDocs/', '', $file),
+                        'src' => $url . $file
+                    ];
+                    }
+            return ['welcome'];
+    }
+
+    public function destroy($image)
+    {
+        Storage::disk('s3')->delete('apiDocs/' . $image);
+        return ['Image was deleted successfully'];
+    }
+
 }
