1afd515246633d82be1b37d2926fdca738eec455
saitarun135 <saitarun135@gm.com>
2021-07-13 21:50:43 +0530

    build: fillable part for orders table

---
+++ app/Models/Orders.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Orders extends Model
+{
+    use HasFactory;
+    protected $table="Orders";
+    protected $fillable = [
+        'customer_id','orderNumber',
+        'order_date'
+    ];
+}
